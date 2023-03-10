cdddcdaf7a6b9c98e7bdd60fee4508f6188e81b8
Pawan Maurya <110979745+Pawan2505@users.noreply.github.com>
2023-03-10 15:16:46 +0530

    Add OOPs File

---
+++ Object Oriented Programming [OOP's]/[OOPs Part - 6] More on C++ Friend Functions (Examples & Explanation)/part2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Y;
+
+class X{
+    int data;
+    public:
+        void setValue(int value){
+            data = value;
+        }
+    friend void add(X, Y);    
+};
+
+class Y{
+    int num;
+    public:
+        void setValue(int value){
+            num = value;
+        }
+    friend void add(X, Y);    
+
+};
+
+void add(X o1, Y o2){
+    cout<<"Summing data of X and Y objects gives me "<< o1.data + o2.num;
+}
+int main(){
+    c1 oc1;
+    c2 oc2;
+
+    oc1.indata(34);
+    oc2.indata(67);
+    exchange(oc1, oc2);
+
+    cout<<"The value of c1 after exchanging becomes: ";
+    oc1.display();
+    cout<<"The value of c2 after exchanging becomes: ";
+    oc2.display();
+    
+    return 0;
+}
